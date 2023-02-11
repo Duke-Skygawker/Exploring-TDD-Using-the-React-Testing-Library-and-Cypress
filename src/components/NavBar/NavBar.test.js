4404a95a8d4117c3812f53afb234da1caeb7b7cc
Bart <82309145+Duke-Skygawker@users.noreply.github.com>
2023-02-11 20:10:08 +0000

    Navbar testing in 60%

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,7 +1,8 @@
 import { render } from "@testing-library/react";
 import NavBar from "./NavBar";
 
-test("The NavBar component should render", () => {
-  const view = render(<NavBar />);
+test("The NavBar component should render with a title", () => {
+  const view = render(<NavBar title="Test Application #2" />);
+
   expect(view).toMatchSnapshot();
 });
