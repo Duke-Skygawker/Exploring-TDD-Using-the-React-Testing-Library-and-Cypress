d9121408c06d7be36041c9c04e0fe9518745f4dd
Duke-Skygawker <bart3kk990@gmail.com>
2023-02-13 17:40:11 +0100

    end to end test added

--- cypress/e2e/hotels.spec.cy.js
+++ cypress/e2e/hotels.spec.cy.js
@@ -4,8 +4,19 @@ describe("Cypress", () => {
 
     cy.get("a").first().click();
     cy.location("pathname").should("include", "hotel");
+  });
 
+  it("navigates to the form to add a review", () => {
     cy.get("[data-cy=addReview]").click();
     cy.location("pathname").should("include", "new");
+  });
+
+  it("fills in and submits the form", () => {
+    cy.get("form").within(() => {
+      cy.get("input[name=title]").type("Test review");
+    });
+    cy.get("input[name=description]").type("Is a test review by Cypress");
+    cy.get("input[name=rating]").type(4);
+    cy.get("button").click();
   });
 });
